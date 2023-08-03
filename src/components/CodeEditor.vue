b851c249ee8b924c8fe1846b6f15ad865131550e
js07 <19861096+js07@users.noreply.github.com>
2023-08-03 17:30:06 -0400

    build: upgrade dependencies

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -6,7 +6,7 @@ import { highlight, languages } from 'prismjs'
 import 'prismjs/components/prism-clike'
 import 'prismjs/components/prism-javascript'
 import 'prismjs/themes/prism-tomorrow.css' // import syntax highlighting styles
-import { computed, withDefaults } from 'vue'
+import { computed } from 'vue'
 
 const props = withDefaults(
   defineProps<{
